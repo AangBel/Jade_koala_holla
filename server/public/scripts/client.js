a7c82ca043a134414b5f8fbec5ac56b7ec5e6152
Angelica Beltran <angelica.beltran11@gmail.com>
2023-08-31 16:18:43 -0500

    added from mistake branch

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,25 +1,28 @@
-console.log( 'js' );
+console.log("js");
 
-$( document ).ready( function(){
-  console.log( 'JQ' );
+$(document).ready(function () {
+  console.log("JQ");
   // Establish Click Listeners
-  setupClickListeners()
+  setupClickListeners();
   // load existing koalas on page load
   getKoalas();
-
 }); // end doc ready
 
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', function(){
-    console.log( 'in addButton on click' );
+  $("#addButton").on("click", function () {
+    console.log("in addButton on click");
     // get user input and put in an object
-    // NOT WORKING YET :(
     // using a test object
-    let koalaNameInput = $('#nameIn').val();
-    let koalaAgeInput = $('#ageIn').val();
-    let koalaGenderInput= $('#genderIn').val();
-    let koalaReady = $('#readyForTransferIn').val();
-    let koalaNotes = $('#notesIn').val();
+    let koalaNameInput = $("#nameIn").val();
+    // console.log(koalaNameInput);
+    let koalaAgeInput = $("#ageIn").val();
+    // console.log(koalaAgeInput);
+    let koalaGenderInput = $("#genderIn").val();
+    // console.log(koalaGenderInput);
+    let koalaReady = $("#readyForTransferIn").val();
+    // console.log(koalaReady);
+    let koalaNotes = $("#notesIn").val();
+    // console.log(koalaNotes);
 
     let koalaToSend = {
       name: koalaNameInput,
@@ -28,19 +31,55 @@ function setupClickListeners() {
       readyForTransfer: koalaReady,
       notes: koalaNotes,
     };
-    // call saveKoala with the new object
-    saveKoala( koalaToSend );
-  }); 
+    // console.log(koalaToSend);
+
+    $.ajax({
+      method: "POST",
+      url: "/koalas",
+      data: koalaToSend,
+    })
+      .then(function (response) {
+        console.log(response);
+        getKoalas;
+      })
+      .catch(function (error) {
+        console.log("error in setup click listeners ajax post", error);
+        alert("Error. Please try again later.");
+      });
+    getKoalas();
+  });
 }
 
-function getKoalas(){
-  console.log( 'in getKoalas' );
+function getKoalas() {
+  console.log("in getKoalas");
+  $("#viewKoalas").empty();
   // ajax call to server to get koalas
-  
+  $.ajax({
+    method: "GET",
+    url: "/koalas",
+  })
+    .then(function (response) {
+      const koalaToReceive = response;
+
+      for (let koala of koalaToReceive) {
+        $("#viewKoalas").append(`
+                <tr>
+                    <td>${koalaToReceive.name}</td>
+                    <td>${koalaToReceive.age}</td>
+                    <td>${koalaToReceive.gender}</td>
+                    <td>${koalaToReceive.ready}</td>
+                    <td>${koalaToReceive.notes}</td>
+                </tr>
+                `);
+      }
+    })
+    .catch(function (error) {
+      console.log("error in koala get", error);
+    });
 } // end getKoalas
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
- 
-}
+// function saveKoala( newKoala ){
+//   console.log( 'in saveKoala', newKoala );
+//   // ajax call to server to get koalas
+
+// }
