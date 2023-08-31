91f6d8b936fb4608c1cd5e26f33a20677e82e29a
Abdirahman Jama <mustalid21@gmail.com>
2023-08-31 15:10:06 -0500

    worked on get request for koala router

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -2,10 +2,26 @@ const express = require('express');
 const koalaRouter = express.Router();
 
 // DB CONNECTION
+const pool = require('../modules/pool.js');
 
 
 // GET
+// router.get('/', (req, res) => {
 
+//     let queryText = 'SELECT * FROM koalas;'
+
+//     pool.query(queryText).then(
+//         (result) => {
+//             res.send(result.rows)
+
+//         }
+//     ).catch(
+//         (err) => {
+//             console.log(`Error making query ${queryText}`, err);
+//             res.sendStatus(500);
+//         }
+//     )
+// });
 
 // POST
 
