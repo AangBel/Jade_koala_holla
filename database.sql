fb74b1856bb05019eaac1b4a0d1b33ba04ed5590
Angelica Beltran <angelica.beltran11@gmail.com>
2023-08-31 18:02:49 -0500

    updated database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "koalas" (
+	"id" SERIAL PRIMARY KEY,
+	"name" varchar(50),
+	"gender" varchar(1),
+	"age" varchar (3),
+	"ready_to_transfer" varchar(1),
+	"notes" varchar (150)
+	);
+
+    
